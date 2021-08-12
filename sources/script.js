8f237b6a4077eefc3696be9e88404651869c4476
nikfine <88845239+nikfine@users.noreply.github.com>
2021-08-12 21:25:15 +0300

    Add user actions in script.js

---
+++ sources/script.js
@@ -0,0 +1,26 @@
+var form = document.querySelector('#feedback_form'),
+    phone = document.querySelector('#phone'),
+    submit = function(e){
+        
+        var ajax = new XMLHttpRequest(),
+            send_data = new FormData(form);
+    
+        ajax.open('POST', '/server_action.php', true);
+        ajax.send(send_data);
+        ajax.onload = function(){
+            console.log(ajax.responseText);
+            alert("Данные загружены");
+        }
+    
+        e.preventDefault();
+    },
+    phone_filter = function(e){
+
+        return e.target.value = e.target.value.replaceAll(/\D/g, '');
+
+    }
+    
+
+form.addEventListener('submit', submit);
+phone.addEventListener('input', phone_filter);
+
